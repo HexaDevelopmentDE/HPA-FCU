53992101de4330d68ef0f92d37f10c1eef7a3178
HexaDevelopmentDE <75783490+HexaDevelopmentDE@users.noreply.github.com>
2024-06-17 10:39:10 +0200

    Add files via upload

---
+++ hpa_fcu_firmware/hpa_fcu_firmware.ino
@@ -0,0 +1,18 @@
+/**
+ * @copyright     (c) 2023 the authors
+ * @author        Konstantin Koark <konstantin.koark@gmx.de>
+ *
+ **/
+
+float valve_open_time = 0.1;
+float rate_of_fire = 30;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
