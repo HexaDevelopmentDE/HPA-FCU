c12e16c449b256cf79e3cd9e77868363a4dc84c2
HexaDevelopmentDE <75783490+HexaDevelopmentDE@users.noreply.github.com>
2024-06-19 10:24:08 +0200

    Add files via upload

--- hpa_fcu_firmware/hpa_fcu_firmware.ino
+++ hpa_fcu_firmware/hpa_fcu_firmware.ino
@@ -2,7 +2,7 @@
  * @copyright     (c) 2024 the author
  * @author        Konstantin Koark <hexa.airsoft@gmail.com>
  *
- * Custom FCU for Airsoft HPA Engines
+ * Custom FCU for Airsoft HPA Engines with 1 valve
  *
  * planned featueres:
  * - adjustable valve opening time
@@ -11,33 +11,101 @@
  * - webinterface for configuration
  **/
 
+#define BUTTON_TRIGGER D3
+#define BUTTON_FIRESELECTOR D8
+
+int prev_trigger_state;
+int prev_selector_state;
+int trigger_state;
+int selector_state;
+
 float valve_open_time = 0.1;
 float rate_of_fire = 30;
 bool binary = false;
-int selectedFireMode = 2;
+int selectedFireModeConfig = 0;
 const char* firemodes[][3] = {{"safe", "semi", "full"},{"safe", "semi", "safe"},{"safe", "semi", "semi"}};
+int currentFireMode = 0;
 
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  pinMode(BUTTON_FIRESELECTOR, INPUT);
+  prev_selector_state = 0;
+  pinMode(BUTTON_TRIGGER, INPUT);
+  prev_trigger_state = 0;
 
   //print out loaded konfiguration
   Serial.println("Valve Opening Time: " + String(valve_open_time));
   Serial.println("Rate of Fire: " + String(rate_of_fire));
   Serial.println("Binary Trigger enabled: " + String(binary));
-  Serial.println("Selected Firemode Konfiguration: " + String(selectedFireMode) + " (" + String(firemodes[selectedFireMode][0]) + ", " + String(firemodes[selectedFireMode][1]) + ", " + String(firemodes[selectedFireMode][2]) + ")");
+  Serial.println("Selected Firemode Konfiguration: " + String(selectedFireModeConfig) + " (" + String(firemodes[selectedFireModeConfig][0]) + ", " + String(firemodes[selectedFireModeConfig][1]) + ", " + String(firemodes[selectedFireModeConfig][2]) + ")");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  switchFireMode();
+  delay(100);
+  cycle();
+}
+
+void switchFireMode() {
+  selector_state = digitalRead(BUTTON_FIRESELECTOR);
+  int oldFireMode = currentFireMode;
+
+  if (selector_state == LOW && prev_selector_state == HIGH) {
+    if(currentFireMode <= 2) {
+      currentFireMode = currentFireMode + 1;
+    }
+    if(currentFireMode == 3) {
+      currentFireMode = 0;
+    }
+    Serial.println("switch fire mode from "  + String(firemodes[selectedFireModeConfig][oldFireMode]) + " to  " + String(firemodes[selectedFireModeConfig][(currentFireMode)]));
+  }
+
+  prev_selector_state = digitalRead(BUTTON_FIRESELECTOR);
 }
 
 //triggers a shot
-void cycle() {}
+void cycle() {
+  trigger_state = digitalRead(BUTTON_TRIGGER);
+
+  if(firemodes[selectedFireModeConfig][currentFireMode] == "safe") {
+    if (trigger_state == LOW && prev_trigger_state == HIGH) {
+      Serial.println("nothing happens, it's on safe");
+    }
+  }
+
+  if(firemodes[selectedFireModeConfig][currentFireMode] == "semi") {
+    if (trigger_state == LOW && prev_trigger_state == HIGH) {
+      Serial.println("triggered");
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(10);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+
+  if(firemodes[selectedFireModeConfig][currentFireMode] == "full") {
+    if (trigger_state == LOW && prev_trigger_state == HIGH) {
+      int end = 0;
+      while(end == 0) {
+        Serial.println("triggered");
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(10);
+        digitalWrite(LED_BUILTIN, LOW);
+        trigger_state = digitalRead(BUTTON_TRIGGER);
+        if(trigger_state == HIGH) {
+          prev_trigger_state = digitalRead(BUTTON_TRIGGER);
+          break;
+        }
+        delay((1000/rate_of_fire));
+      }
+    }
+  }
+
+  prev_trigger_state = digitalRead(BUTTON_TRIGGER);  
+}
 
 void startWebServer() {}
 
